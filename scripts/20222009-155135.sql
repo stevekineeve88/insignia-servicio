f8e6a14c509e2dc36fcef9704f9da3c64587f616
stevekineeve88 <stevemamajama@gmail.com>
2022-09-20 16:26:20 -0400

    added framework and migration scripts

---
+++ scripts/20222009-155135.sql
@@ -0,0 +1,25 @@
+CREATE TABLE action (
+  id INT NOT NULL AUTO_INCREMENT,
+  const VARCHAR(100) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX `action_const_UNIQUE` (`const` ASC)
+);
+
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  const VARCHAR(100) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX `role_const_UNIQUE` (`const` ASC)
+);
+
+CREATE TABLE role_group (
+  id INT NOT NULL AUTO_INCREMENT,
+  uuid BINARY(16) DEFAULT (uuid_to_bin(uuid())),
+  const VARCHAR(100) NOT NULL,
+  description VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE INDEX `role_group_const_UNIQUE` (`const` ASC),
+  UNIQUE INDEX `role_group_uuid_UNIQUE` (`uuid` ASC)
+);
---
+++ service_locator.py
@@ -0,0 +1,19 @@
+from sk88_service_locator.modules.service.managers.service_manager import ServiceManager
+from modules.util.config.config import UtilConfig
+
+
+service_locator: ServiceManager or None = None
+
+
+def get_service_manager() -> ServiceManager:
+    """ Get service manager
+    Returns:
+        ServiceManager
+    """
+    global service_locator
+
+    if service_locator is None:
+        service_locator = ServiceManager()
+        service_locator.add(UtilConfig().get())
+
+    return service_locator
